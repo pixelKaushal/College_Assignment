6043f0d0f735fe735701f3244f62a5491a21c3eb
KaushalLMAO <gautamkaushal381@gmail.com>
2024-12-06 14:50:21 +0000

    Refactor Factorial.c to improve readability and add input validation

--- Factorial.c
+++ Factorial.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+
 int main() {
     int a, i;
     int b = 1;
-    printf("Enter a number: ");
-    scanf("%d", &a);
+    printf("Enter a positive integer: ");
+    if (scanf("%d", &a) != 1 || a < 0) {
+        printf("Invalid input. Please enter a positive integer.\n");
+        return 1;
+    }
     for(i = a; i >= 1; i--) {
         b *= i;
     }
-    printf(" %d\n",b);
+    printf("Factorial of %d is %d\n", a, b);
     return 0;
 }
 
